96389f168bee159d378c4388887a2367816035c6
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-07 13:36:48 +0545

    feat: game over logic

---
+++ src/scripts/gameOver.js
@@ -0,0 +1,35 @@
+let gameOver = false;
+let restartGame = false; // New flag to indicate whether to restart the game
+
+function setGameOver() {
+    gameOver = true;
+}
+
+function resetGame() {
+    gameOver = false;
+    restartGame = false; // Reset the restartGame flag
+}
+
+function isGameOver() {
+    return gameOver;
+}
+
+function setRestartGame() {
+    restartGame = true;
+}
+
+function shouldRestartGame() {
+    return restartGame;
+}
+
+
+function drawGameOverScreen(ctx, canvas) {
+    if (isGameOver()) {
+        ctx.fillStyle = 'blue';
+        ctx.font = '40px Arial';
+        ctx.fillText('Game Over', canvas.width / 2 - 100, canvas.height / 2 - 20);
+        ctx.font = '20px Arial';
+        ctx.fillText('Press Space to Restart', canvas.width / 2 - 130, canvas.height / 2 + 20);
+    }
+}
+
