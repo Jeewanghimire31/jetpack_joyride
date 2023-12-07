9de6142ad8012af877dc1075a52d0de1b5c35dfc
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-07 17:20:30 +0545

    fix: eventlistner of space

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,33 +1,31 @@
 
 let vx = 0;
 let vy = 0;
+let currentSpeed = SPEED;
 
 addEventListener("keydown", (e)=>{
-    if(e.code == 'KeyD') vx=SPEED;
-    if(e.code == 'KeyA') vx=-SPEED;
-    if(e.code == 'KeyW') vy=-SPEED;
-    if(e.code == 'KeyS') vy=SPEED;
+    if (isGameOver()) return; // Don't handle input if the game is over
+
+    if(e.code == 'KeyD') vx=currentSpeed;
+    if(e.code == 'KeyA') vx=-currentSpeed;
+    if(e.code == 'KeyW') vy=-currentSpeed;
+    if(e.code == 'KeyS') vy=currentSpeed;
     // console.log(e.code);
     // console.log(vx);
 
-    if (e.code === 'Space') {
-        if (shouldRestartGame()) {
-            // If should restart, reset the game
-            resetGame();
-            player.x = 0;
-            player.y = 0;
-            obstacleManager.reset();
-            animate();
-        } else {
-            // Otherwise, set the restart flag
-            setRestartGame();
-        }
-    }
 });
-
 addEventListener("keyup", (e)=>{
-    if(e.code == 'KeyD') vx=0;
-    if(e.code == 'KeyA') vx=0;  
+        // if(e.code == 'KeyD') vx=0;
+    // if(e.code == 'KeyA') vx=0;  
     if(e.code == 'KeyW') vy=0;
     if(e.code == 'KeyS') vy=0;
+if (e.code === 'Space') {
+    if (shouldRestartGame()) {
+        // If should restart, reset the game
+        resetGame();
+    } else {
+        // Otherwise, set the restart flag
+        setRestartGame();
+    }
+}
 });
