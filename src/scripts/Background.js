27a7e792ebe0d6423981f34e7c2effdd6b5e49bb
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-07 17:18:46 +0545

    feat: background image movement

---
+++ src/scripts/Background.js
@@ -0,0 +1,32 @@
+
+class Background {
+    
+    constructor(canvas, ctx) {
+        
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.speed = 1;
+        this.position = 0;
+        this.bgImage = new Image();
+        this.bgImage.src = "background.png";
+    }
+
+    update() {
+        this.position -= this.speed;
+        
+        // If the first image is completely off-screen to the left, reset its position
+        if (this.position <= -this.canvas.width) {
+            this.position = 0;
+        }
+    }
+
+    draw() {
+        // Draw the image with the x-coordinate based on the position
+        this.ctx.drawImage(this.bgImage, this.position, 0);
+
+        // Draw a second copy of the image to cover the space when the first image moves off-screen
+        this.ctx.drawImage(this.bgImage, this.position + this.canvas.width, 0);
+    }
+}
+
+
