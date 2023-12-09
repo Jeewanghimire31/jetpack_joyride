b72544e69561583502c27616f3272ae6074937a0
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-09 15:59:19 +0545

    feat: coins appear randomly and when collide with character get remove

--- src/scripts/ObstacleFunction.js
+++ src/scripts/ObstacleFunction.js
@@ -1,9 +1,16 @@
 class ObstacleManager {
-    constructor(count, width, height, canvasWidth, canvasHeight, speed) {
+    constructor(count, width, height, canvasWidth, canvasHeight, speed, interval) {
         this.obstacles = generateRandomObstacles(count, width, height, canvasWidth, canvasHeight);
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
         this.speed = speed;
+
+        // Add coins property
+    this.coins = [];
+    this.coinInterval = interval;
+    this.lastCoinTime = Date.now();
+    this.coinGroupX = canvasWidth; // Initial X position for the coin group
+        this.coinGroupY = getRandomNum(0, canvasHeight - 20);
     }
 
     // drawing obstacles which were generated in obstacle.js file
@@ -46,6 +53,40 @@ class ObstacleManager {
                 obstacle.y = getRandomNum(0, this.canvasHeight - obstacle.height); // Reset to a random y-coordinate
             }
         });
+
+            // Move existing coins to the left
+            this.coins.forEach((coin) => {
+                coin.x -= this.speed; // Assuming SPEED is a constant for the scrolling speed
+            });
+    
+            // Remove coins that have gone off the left side of the canvas
+            this.coins = this.coins.filter((coin) => coin.x + coin.width > 0);
+    
+            // Generate new coins when there are no coins on the screen
+        if (this.coins.length === 0) {
+            this.generateCoins();
+        }
+        
+    }
+
+    generateCoins() {
+        const currentTime = Date.now();
+        if (currentTime - this.lastCoinTime > this.coinInterval) {
+            for (let i = 0; i < this.coinGroupSize; i++) {
+                const coin = new Coin(
+                    this.coinGroupX + getRandomNum(0, 50), // Adjust the range based on your requirements
+                    this.coinGroupY + getRandomNum(0, 50), // Adjust the range based on your requirements
+                    20,
+                    20
+                );
+                this.coins.push(coin);
+            }
+          
+            this.lastCoinTime = currentTime;
+            this.coinGroupSize = getRandomNum(5, 10);
+            this.coinGroupX = canvas.width; // Reset X position for the next coin group
+            this.coinGroupY = getRandomNum(0, canvas.height - 20); // Reset Y position for the next coin group
+        }
     }
 
     reset() {
