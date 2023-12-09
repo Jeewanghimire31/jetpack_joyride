16d51c5cf66dfc5a54f2b0200a4e88645c140293
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-09 09:07:27 +0545

    fix: two obstacles never collide within

--- src/scripts/ObstacleFunction.js
+++ src/scripts/ObstacleFunction.js
@@ -1,39 +1,3 @@
-
-
-// class ObstacleManager {
-//     constructor(count, width, height, canvasWidth, canvasHeight) {
-//         this.obstacles = generateRandomObstacles(count, width, height, canvasWidth, canvasHeight);
-
-//         this.speed = SPEED;
-//     }
-
-//     draw(ctx) {
-//         this.obstacles.forEach(function(obstacle) {
-//             obstacle.draw(ctx);
-//           });
-//     }
-
-//     update() {
-//         this.obstacles.forEach((obstacle) => {
-//             obstacle.x -= this.speed;
-
-//             // If the obstacle is completely off-screen to the left, reset its position
-//             if (obstacle.x + obstacle.width < 0) {
-//                 obstacle.x = this.canvasWidth; // Reset to the right side of the canvas
-//                 obstacle.y = getRandomNum(0, this.canvasHeight - obstacle.height); // Reset to a random y-coordinate
-//             }
-//         });
-//     }
-
-//     reset() {
-//         this.obstacles = generateRandomObstacles(this.obstacles.length, 30, 30, canvas.width, canvas.height);
-//     }
-// }
-
-
-
-
-
 class ObstacleManager {
     constructor(count, width, height, canvasWidth, canvasHeight, speed) {
         this.obstacles = generateRandomObstacles(count, width, height, canvasWidth, canvasHeight);
@@ -42,12 +6,36 @@ class ObstacleManager {
         this.speed = speed;
     }
 
+    // drawing obstacles which were generated in obstacle.js file
     draw(ctx) {
         this.obstacles.forEach(function (obstacle) {
+            while (this.isTooCloseToOtherObstacles(obstacle)) {
+                obstacle.x = getRandomNum(0, this.canvasWidth - obstacle.width);
+                obstacle.y = getRandomNum(0, this.canvasHeight - obstacle.height);
+            }
+
             obstacle.draw(ctx);
-        });
+        }, this); 
     }
 
+    // checking that random generated obstacles are near to each other or not
+    isTooCloseToOtherObstacles(obstacle) {
+        for (const otherObstacle of this.obstacles) {
+            if (obstacle !== otherObstacle) {
+                const distance = Math.abs(obstacle.x - otherObstacle.x) +
+                                 Math.abs(obstacle.y - otherObstacle.y);
+
+                // Adjust the threshold as needed
+                if (distance < 100) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    
     update() {
         this.obstacles.forEach((obstacle) => {
             obstacle.x -= this.speed;
