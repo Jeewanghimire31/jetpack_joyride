f0e8703073c231cffc862413c0bea23e9c5bdfe4
Jeewanghimire31 <jeewanghimire31@gmail.com>
2023-12-14 14:08:05 +0545

    fix: background height fix with platform impemented

--- src/scripts/Background.js
+++ src/scripts/Background.js
@@ -6,7 +6,8 @@ class Background {
         this.canvas = canvas;
         this.ctx = ctx;
         this.speed = 5;
-        this.position = 0;
+        this.x = 0;
+        this.y = this.canvas.height-100;
         this.bgImage = new Image();
         this.bgImage.src = "background.png";
         this.increaseDistance = increaseDistance;
@@ -14,23 +15,23 @@ class Background {
 
     update() {
         if(gameOver) return;
-        this.position -= this.speed;
+        this.x -= this.speed;
 
          // Call the increaseDistance function
          this.increaseDistance(1);
         
-        // If the first image is completely off-screen to the left, reset its position
-        if (this.position <= -this.canvas.width) {
-            this.position = 0;
+        // If the first image is completely off-screen to the left, reset its x
+        if (this.x <= -this.canvas.width) {
+            this.x = 0;
         }
     }
 
     draw() {
-        // Draw the image with the x-coordinate based on the position
-        this.ctx.drawImage(this.bgImage, this.position, 0);
+        // Draw the image with the x-coordinate based on the x
+        this.ctx.drawImage(this.bgImage, this.x, 0, this.canvas.width, this.y);
 
         // Draw a second copy of the image to cover the space when the first image moves off-screen
-        this.ctx.drawImage(this.bgImage, this.position + this.canvas.width, 0);
+        this.ctx.drawImage(this.bgImage, this.x + this.canvas.width, 0, this.canvas.width, this.y);
     }
 }
 
